b975cede2c70e1e5f542d1d5e141244d65ed0be6
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-12 15:17:21 +0200

    Add route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from'./components/dashboard/Dashboard';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/dashboard" component={Dashboard} />
         </div>
       </Router>
     </Provider>
