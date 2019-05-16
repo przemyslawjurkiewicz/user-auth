6b9e9108f2f2169f1d8e490860fe989dadd2f0cd
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-16 09:47:10 +0200

    Fix keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://jurczas1:Mjmsod2011!@cluster0-sukdp.mongodb.net/test?retryWrites=tru',
+    'mongodb+srv://bduser:Mjmsod2011@cluster0-sukdp.mongodb.net/test?retryWrites=tru',
   secretOrKey: 'secret'
 };
